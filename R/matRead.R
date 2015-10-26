c602ac9904a10c610f27c0fde826c8e92aed7190
Daniel Vera <dlv04c@spear-14.rcc.fsu.edu>
2015-10-26 12:31:47 -0400

    add option to matRead to keep single mat as list

--- R/matRead.R
+++ R/matRead.R
@@ -1,8 +1,8 @@
-matRead <- function( mat, fmat=FALSE , threads=getOption("threads",1L), ... ){
+matRead <- function( mat, fmat=FALSE , threads=getOption("threads",1L), unlistSingle=TRUE , ... ){
 	#if(!fmat){
     m <- mclapply(mat,read.tsv, row.names=1 , mc.cores=threads, mc.preschedule=FALSE,  ... )
 		m <- lapply(m, as.matrix)
-		if(length(m)==1){m<-m[[1]]}
+		if(length(m)==1 & unlistSingle){m<-m[[1]]}
     return(m)
   # } else{
   #   as.matrix(read.table(mat,stringsAsFactors=FALSE,sep="\t",row.names=1, ... ))
